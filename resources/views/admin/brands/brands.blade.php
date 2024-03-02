4549c9f8785313835cec046cf61d1b08f67cde98
dipanjang7286 <71178502+dipanjang7286@users.noreply.github.com>
2024-03-02 15:09:10 +0530

    category sub-category brand status fixed

--- resources/views/admin/brands/brands.blade.php
+++ resources/views/admin/brands/brands.blade.php
@@ -43,11 +43,20 @@
                     <td>{{$key+1}}</td>
                     <td>{{$value->name}}</td>
                     <td>{{$value->slug}}</td>
-                    <td>
+                    <td class="align-middle text-center">
                         @if ($value->status == 1)
-                            {{"Active"}}
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                <!-- Success Tick Mark -->
+                                <circle cx="12" cy="12" r="11" fill="none" stroke="#63E6BE" stroke-width="2"/>
+                                <path d="M7 13l3 3 7-7" stroke="#63E6BE" stroke-width="2" fill="none" />
+                            </svg>
                         @else
-                            {{"In-Active"}}
+                        
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                            <!-- Cross -->
+                                <circle cx="12" cy="12" r="11" fill="none" stroke="#FF6B6B" stroke-width="2"/>
+                                <path d="M12 10.586l4.95-4.95c.39-.39 1.024-.39 1.414 0s.39 1.024 0 1.414L13.414 12l4.95 4.95c.39.39.39 1.024 0 1.414s-1.024.39-1.414 0L12 13.414l-4.95 4.95c-.39.39-1.024.39-1.414 0s-.39-1.024 0-1.414L10.586 12 5.636 7.05c-.39-.39-.39-1.024 0-1.414s1.024-.39 1.414 0L12 10.586z" fill="#FF6B6B"/>
+                            </svg>
                         @endif
                         
                     </td>
